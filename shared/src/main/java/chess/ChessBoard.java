f714832ad76e109e9767f7f4e406d49b19648ac8
hall-grant <hall.grant.david@gmail.com>
2026-01-23 23:30:31 -0600

    Rewrote PieceMovesCalculator, fixed resetBoard.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -41,14 +44,18 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
+
+        // reset the board from scratch:
+        board = new ChessPiece[8][8];
+
         // white pawns
         for(int c = 1; c <= 8; c++){ // C++ reference ?????!!!!1!!!!
-            addPiece(new ChessPosition(1,c),new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+            addPiece(new ChessPosition(2,c),new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
         }
 
         // black pawns
         for(int c = 1; c <= 8; c++){
-            addPiece(new ChessPosition(6,c),new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
+            addPiece(new ChessPosition(7,c),new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
         }
 
         // rooks
@@ -79,4 +86,24 @@ public class ChessBoard {
 
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(board, that.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(board);
+    }
+
+    @Override
+    public String toString() {
+        return "ChessBoard{" +
+                "board=" + Arrays.toString(board) +
+                '}';
+    }
 }
--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -29,6 +30,363 @@ public class PieceMovesCalculator {
         return collection;
     }
 
+    // general purpose, should be much more readable
+    private static ChessPosition offset(ChessPosition start, int row, int col){
+        int r = start.getRow() + row;
+        int c = start.getColumn() + col;
+        if(r >= 1 && r <= 8 && c >= 1 && c <= 8) return new ChessPosition(r, c);
+        return null;
+    }
+
+    private static Collection<ChessMove> pawnCalculator(ChessBoard board, ChessPosition firstPos, ChessPiece piece){
+        Collection<ChessMove> collection = new ArrayList<>();
+
+        // white pawns
+        if(piece.getTeamColor() == ChessGame.TeamColor.WHITE){
+            ChessPosition upOne = offset(firstPos, 1, 0);
+            if(upOne != null && board.getPiece(upOne) == null){
+                collection.add(new ChessMove(firstPos, upOne, null));
+                if(firstPos.getRow() == 2){
+                    ChessPosition upTwo = offset(firstPos, 2, 0);
+                    if(upTwo != null && board.getPiece(upTwo) == null){
+                        collection.add(new ChessMove(firstPos, upTwo, null));
+                    }
+                }
+            }
+        }
+
+        // white diagonals
+        // diagonal left
+        ChessPosition upLeft = offset(firstPos, 1, -1);
+        if(upLeft != null){
+            if (board.getPiece(upLeft) != null && board.getPiece(upLeft).getTeamColor() == ChessGame.TeamColor.BLACK){
+                collection.add(new ChessMove(firstPos, upLeft, null));
+            }
+        }
+        // diagonal right
+        ChessPosition upRight = offset(firstPos, 1, 1);
+        if(upRight != null){
+            if(board.getPiece(upRight) != null && board.getPiece(upRight).getTeamColor() == ChessGame.TeamColor.BLACK){
+                collection.add(new ChessMove(firstPos, upRight, null));
+            }
+        }
+
+
+        // black pawns
+        if(piece.getTeamColor() == ChessGame.TeamColor.BLACK){
+            ChessPosition downOne = offset(firstPos, -1, 0);
+            if(downOne != null && board.getPiece(downOne) == null){
+                collection.add(new ChessMove(firstPos, downOne, null));
+                if(firstPos.getRow() == 7){
+                    ChessPosition downTwo = offset(firstPos, -2, 0);
+                    if(downTwo != null && board.getPiece(downTwo) == null){
+                        collection.add(new ChessMove(firstPos, downTwo, null));
+                    }
+                }
+            }
+        }
+
+        // black diagonals
+        // diagonal left
+        ChessPosition downLeft = offset(firstPos, -1, -1);
+        if(downLeft != null){
+            if (board.getPiece(downLeft) != null && board.getPiece(downLeft).getTeamColor() == ChessGame.TeamColor.WHITE){
+                collection.add(new ChessMove(firstPos, downLeft, null));
+            }
+        }
+
+        // diagonal right
+        ChessPosition downRight = offset(firstPos, -1, 1);
+        if(downRight != null){
+            if(board.getPiece(downRight) != null && board.getPiece(downRight).getTeamColor() == ChessGame.TeamColor.WHITE){
+                collection.add(new ChessMove(firstPos, downRight, null));
+            }
+        }
+
+        return collection;
+    }
+
+
+    private static Collection<ChessMove> rookCalculator(ChessBoard board, ChessPosition firstPos, ChessPiece piece){
+        Collection<ChessMove> collection = new ArrayList<>();
+
+        // up
+        for (int i = 1; i <= 8; i++){
+            ChessPosition n = offset(firstPos, i, 0);
+            if(n == null) break;
+
+            if(board.getPiece(n) == null) collection.add(new ChessMove(firstPos, n, null));
+            else{
+                if(board.getPiece(n).getTeamColor() != piece.getTeamColor()) collection.add(new ChessMove(firstPos, n, null));
+                break;
+            }
+        }
+
+        // down
+        for (int i = 1; i <= 8; i++){
+            ChessPosition n = offset(firstPos, -i, 0);
+            if(n == null) break;
+
+            if(board.getPiece(n) == null) collection.add(new ChessMove(firstPos, n, null));
+            else{
+                if(board.getPiece(n).getTeamColor() != piece.getTeamColor()) collection.add(new ChessMove(firstPos, n, null));
+                break;
+            }
+        }
+
+        // right
+        for (int i = 1; i <= 8; i++){
+            ChessPosition n = offset(firstPos, 0, i);
+            if(n == null) break;
+
+            if(board.getPiece(n) == null) collection.add(new ChessMove(firstPos, n, null));
+            else{
+                if(board.getPiece(n).getTeamColor() != piece.getTeamColor()) collection.add(new ChessMove(firstPos, n, null));
+                break;
+            }
+        }
+
+        // left
+        for (int i = 1; i <= 8; i++){
+            ChessPosition n = offset(firstPos, 0, -i);
+            if(n == null) break;
+
+            if(board.getPiece(n) == null) collection.add(new ChessMove(firstPos, n, null));
+            else{
+                if(board.getPiece(n).getTeamColor() != piece.getTeamColor()) collection.add(new ChessMove(firstPos, n, null));
+                break;
+            }
+        }
+
+        return collection;
+
+    }
+
+
+    private static Collection<ChessMove> bishopCalculator(ChessBoard board, ChessPosition firstPos, ChessPiece piece){
+        Collection<ChessMove> collection = new ArrayList<>();
+
+        // up right
+        for(int i = 1; i <= 8; i++){
+            ChessPosition n = offset(firstPos, i, i);
+            if(n == null) break;
+
+            if(board.getPiece(n) == null) collection.add(new ChessMove(firstPos, n, null));
+            else{
+                if(board.getPiece(n).getTeamColor() != piece.getTeamColor()) collection.add(new ChessMove(firstPos, n, null));
+                break;
+            }
+        }
+
+        // up left
+        for(int i = 1; i <= 8; i++){
+            ChessPosition n = offset(firstPos, i, -i);
+            if(n == null) break;
+
+            if(board.getPiece(n) == null) collection.add(new ChessMove(firstPos, n, null));
+            else{
+                if(board.getPiece(n).getTeamColor() != piece.getTeamColor()) collection.add(new ChessMove(firstPos, n, null));
+                break;
+            }
+        }
+
+        // down righit
+        for(int i = 1; i <= 8; i++){
+            ChessPosition n = offset(firstPos, -i, i);
+            if(n == null) break;
+
+            if(board.getPiece(n) == null) collection.add(new ChessMove(firstPos, n, null));
+            else{
+                if(board.getPiece(n).getTeamColor() != piece.getTeamColor()) collection.add(new ChessMove(firstPos, n, null));
+                break;
+            }
+        }
+
+        // down left
+        for(int i = 1; i <= 8; i++){
+            ChessPosition n = offset(firstPos, -i, -i);
+            if(n == null) break;
+
+            if(board.getPiece(n) == null) collection.add(new ChessMove(firstPos, n, null));
+            else{
+                if(board.getPiece(n).getTeamColor() != piece.getTeamColor()) collection.add(new ChessMove(firstPos, n, null));
+                break;
+            }
+        }
+
+        return collection;
+
+    }
+
+
+    private static Collection<ChessMove> queenCalculator(ChessBoard board, ChessPosition firstPos, ChessPiece piece){
+        Collection<ChessMove> collection = new ArrayList<>();
+
+        collection.addAll(rookCalculator(board, firstPos, piece));
+        collection.addAll(bishopCalculator(board, firstPos, piece));
+
+        return collection;
+
+    }
+
+
+    private static Collection<ChessMove> kingCalculator(ChessBoard board, ChessPosition firstPos, ChessPiece piece) {
+        Collection<ChessMove> collection = new ArrayList<>();
+
+        ChessPosition n;
+
+        // up
+        n = offset(firstPos, 1, 0);
+        if(n != null) {
+            if (board.getPiece(n) == null) collection.add(new ChessMove(firstPos, n, null));
+            else {
+                if (board.getPiece(n).getTeamColor() != piece.getTeamColor()) collection.add(new ChessMove(firstPos, n, null));
+            }
+        }
+
+        // down
+        n = offset(firstPos, -1, 0);
+        if(n != null) {
+            if (board.getPiece(n) == null) collection.add(new ChessMove(firstPos, n, null));
+            else {
+                if (board.getPiece(n).getTeamColor() != piece.getTeamColor()) collection.add(new ChessMove(firstPos, n, null));
+            }
+        }
+
+        // right
+        n = offset(firstPos, 0, 1);
+        if(n != null) {
+            if (board.getPiece(n) == null) collection.add(new ChessMove(firstPos, n, null));
+            else {
+                if (board.getPiece(n).getTeamColor() != piece.getTeamColor()) collection.add(new ChessMove(firstPos, n, null));
+            }
+        }
+
+        // left
+        n = offset(firstPos, 0, -1);
+        if(n != null) {
+            if (board.getPiece(n) == null) collection.add(new ChessMove(firstPos, n, null));
+            else {
+                if (board.getPiece(n).getTeamColor() != piece.getTeamColor()) collection.add(new ChessMove(firstPos, n, null));
+            }
+        }
+
+        // up right
+        n = offset(firstPos, 1, 1);
+        if(n != null) {
+            if (board.getPiece(n) == null) collection.add(new ChessMove(firstPos, n, null));
+            else {
+                if (board.getPiece(n).getTeamColor() != piece.getTeamColor()) collection.add(new ChessMove(firstPos, n, null));
+            }
+        }
+
+        // up left
+        n = offset(firstPos, 1, -1);
+        if(n != null) {
+            if (board.getPiece(n) == null) collection.add(new ChessMove(firstPos, n, null));
+            else {
+                if (board.getPiece(n).getTeamColor() != piece.getTeamColor()) collection.add(new ChessMove(firstPos, n, null));
+            }
+        }
+
+        // down right
+        n = offset(firstPos, -1, 1);
+        if(n != null) {
+            if (board.getPiece(n) == null) collection.add(new ChessMove(firstPos, n, null));
+            else {
+                if (board.getPiece(n).getTeamColor() != piece.getTeamColor()) collection.add(new ChessMove(firstPos, n, null));
+            }
+        }
+
+        // down left
+        n = offset(firstPos, -1, -1);
+        if(n != null) {
+            if (board.getPiece(n) == null) collection.add(new ChessMove(firstPos, n, null));
+            else {
+                if (board.getPiece(n).getTeamColor() != piece.getTeamColor()) collection.add(new ChessMove(firstPos, n, null));
+            }
+        }
+
+        return collection;
+    }
+
+
+    private static Collection<ChessMove> knightCalculator(ChessBoard board, ChessPosition firstPos, ChessPiece piece) {
+        Collection<ChessMove> collection = new ArrayList<>();
+
+        ChessPosition n;
+
+        // position 1 (up 2 right 1)
+        n = offset(firstPos, 2, 1);
+        if (n != null) {
+            if (board.getPiece(n) == null || board.getPiece(n).getTeamColor() != piece.getTeamColor()) {
+                collection.add(new ChessMove(firstPos, n, null));
+            }
+        }
+
+        // position 2 (up 1 right 2)
+        n = offset(firstPos, 1, 2);
+        if (n != null) {
+            if (board.getPiece(n) == null || board.getPiece(n).getTeamColor() != piece.getTeamColor()) {
+                collection.add(new ChessMove(firstPos, n, null));
+            }
+        }
+
+        n = offset(firstPos, -1, 2);
+        if (n != null) {
+            if (board.getPiece(n) == null || board.getPiece(n).getTeamColor() != piece.getTeamColor()) {
+                collection.add(new ChessMove(firstPos, n, null));
+            }
+        }
+
+        n = offset(firstPos, -2, 1);
+        if (n != null) {
+            if (board.getPiece(n) == null || board.getPiece(n).getTeamColor() != piece.getTeamColor()) {
+                collection.add(new ChessMove(firstPos, n, null));
+            }
+        }
+
+        n = offset(firstPos, -2, -1);
+        if (n != null) {
+            if (board.getPiece(n) == null || board.getPiece(n).getTeamColor() != piece.getTeamColor()) {
+                collection.add(new ChessMove(firstPos, n, null));
+            }
+        }
+
+        n = offset(firstPos, -1, -2);
+        if (n != null) {
+            if (board.getPiece(n) == null || board.getPiece(n).getTeamColor() != piece.getTeamColor()) {
+                collection.add(new ChessMove(firstPos, n, null));
+            }
+        }
+
+        n = offset(firstPos, 1, -2);
+        if (n != null) {
+            if (board.getPiece(n) == null || board.getPiece(n).getTeamColor() != piece.getTeamColor()) {
+                collection.add(new ChessMove(firstPos, n, null));
+            }
+        }
+
+        // position 8
+        n = offset(firstPos, 2, -1);
+        if (n != null) {
+            if (board.getPiece(n) == null || board.getPiece(n).getTeamColor() != piece.getTeamColor()) {
+                collection.add(new ChessMove(firstPos, n, null));
+            }
+        }
+
+        return collection;
+
+    }
+
+
+
+
+
+}
+
+
+/*
     private static Collection<ChessMove> pawnCalculator(ChessBoard board, ChessPosition firstPos, ChessPiece piece){
         Collection<ChessMove> collection = new ArrayList<>();
         ChessPosition checkedPos;
@@ -365,14 +723,18 @@ public class PieceMovesCalculator {
         }
     }
 
-    /**
-     * Checks possible moves for Knight pieces. Count can be 1-8, beginning with top right and going clockwise to top left.
-     * @param board a chessBoard object with the piece on it
-     * @param firstPos starting position of the piece
-     * @param piece the piece to move
-     * @param count which of the valid 8 spaces to move (1 for top-right, clockwise until 8 for top-left)
-     * @return a valid position or the initial position if invalid.
+
      */
+
+/*
+ * Checks possible moves for Knight pieces. Count can be 1-8, beginning with top right and going clockwise to top left.
+ * @param board a chessBoard object with the piece on it
+ * @param firstPos starting position of the piece
+ * @param piece the piece to move
+ * @param count which of the valid 8 spaces to move (1 for top-right, clockwise until 8 for top-left)
+ * @return a valid position or the initial position if invalid.
+ */
+    /*
     private static ChessPosition checkKnightDirections(ChessBoard board, ChessPosition firstPos, ChessPiece piece, int count){
         // check bounds
         if(count == 0){
@@ -431,4 +793,6 @@ public class PieceMovesCalculator {
         }
 
     }
-}
+
+
+     */
