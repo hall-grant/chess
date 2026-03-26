d905fdf080040caf0f9635d6ca8f038d31262e9a
hall-grant <hall.grant.david@gmail.com>
2026-03-26 00:39:18 -0600

    Main loops in Client mostly done, stub methods to fill out later.

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -19,10 +19,7 @@ public class Client {
         out.println("loop started");
         Scanner scanner = new Scanner(System.in);
 
-        String result = "";
 
-
-        // break later. Maybe have one or both of the loops return a boolean to kill the loop
         while (true){
             if(authToken == null){
                 if(preLogin(scanner)){
@@ -41,12 +38,12 @@ public class Client {
     private Boolean preLogin(Scanner scanner) {
         System.out.println("Hello. Pre-login:");
 
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("1 - Login\n")
-                .append("2 - Register\n")
-                .append("3 - Help\n")
-                .append("4 - quit\n");
-        System.out.print(stringBuilder);
+        String message =
+                "1 - Login\n" +
+                "2 - Register\n" +
+                "3 - Help\n" +
+                "4 - quit\n";
+        System.out.print(message);
 
         String input = scanner.nextLine();
 
@@ -74,6 +71,7 @@ public class Client {
 
     private void login() {
         System.out.println("logging in");
+        authToken = "p";
     }
 
     private void register() {
@@ -82,7 +80,69 @@ public class Client {
 
     private boolean postLogin(Scanner scanner) {
 
+        System.out.println("Hello. Post-login:");
+
+        String message =
+                "1 - logout\n" +
+                "2 - list games\n" +
+                "3 - create a game\n" +
+                "4 - play a game\n" +
+                "5 - observe a game\n" +
+                "6 - help\n";
+
+        System.out.print(message);
+
+        String input = scanner.nextLine();
+
+        switch(input){
+            case "1":
+                logout();
+                break;
+            case "2":
+                list();
+                break;
+            case "3":
+                create();
+                break;
+            case "4":
+                join();
+                break;
+            case "5":
+                observe();
+                break;
+            case "6":
+                postLoginHelp();
+                break;
+            default:
+                System.out.println("Invalid command. Enter 1, 2, 3, 4, 5, or 6.");
+        }
+
         return false;
     }
+
+    private void postLoginHelp() {
+
+    }
+
+    private void observe() {
+
+    }
+
+    private void logout() {
+
+    }
+
+    private void list() {
+
+    }
+
+    private void create() {
+
+    }
+
+    private void join() {
+
+    }
+
 
 }
