4c76fedf83f80125ad6fc4a0db154de661b9b4bf
hall-grant <hall.grant.david@gmail.com>
2026-03-27 18:16:35 -0600

    register tests work

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -5,8 +5,8 @@ import ui.Client;
 
 public class ClientMain {
     public static void main(String[] args) {
-//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-//        System.out.println("♕ 240 Chess Client: " + piece);
+        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        System.out.println("♕ 240 Chess Client: " + piece);
 
         int port = 8080;
         if(args.length > 0){
--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -2,17 +2,23 @@ package client;
 
 import org.junit.jupiter.api.*;
 import server.Server;
+import records.*;
+import ui.ServerFacade;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade serverFacade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        serverFacade = new ServerFacade(port);
     }
 
     @AfterAll
@@ -20,10 +26,25 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    void clearDatabase() throws Exception{
+        serverFacade.clear();
+    }
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void registerPositive() throws Exception{
+        RegisterRequest req = new RegisterRequest("reg1", "pass", "email");
+        RegisterResult res = serverFacade.register(req);
+
+        assertNotNull(res.authToken());
+    }
+
+    @Test
+    public void registerNegative() throws Exception{
+        var req = new RegisterRequest("reg1", "pass", "email");
+        var res = serverFacade.register(req);
+
+        assertThrows(Exception.class, () -> serverFacade.register(req)); // register twice
     }
 
 }
