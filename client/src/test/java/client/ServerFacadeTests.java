f7f45c25ce1fae249597dfea97a70a58c0e11909
hall-grant <hall.grant.david@gmail.com>
2026-03-27 18:21:02 -0600

    login tests work

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -11,14 +11,14 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ServerFacadeTests {
 
     private static Server server;
-    private static ServerFacade serverFacade;
+    private static ServerFacade sf;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
-        serverFacade = new ServerFacade(port);
+        sf = new ServerFacade(port);
     }
 
     @AfterAll
@@ -28,13 +28,13 @@ public class ServerFacadeTests {
 
     @BeforeEach
     void clearDatabase() throws Exception{
-        serverFacade.clear();
+        sf.clear();
     }
 
     @Test
     public void registerPositive() throws Exception{
         RegisterRequest req = new RegisterRequest("reg1", "pass", "email");
-        RegisterResult res = serverFacade.register(req);
+        RegisterResult res = sf.register(req);
 
         assertNotNull(res.authToken());
     }
@@ -42,9 +42,24 @@ public class ServerFacadeTests {
     @Test
     public void registerNegative() throws Exception{
         var req = new RegisterRequest("reg1", "pass", "email");
-        var res = serverFacade.register(req);
+        var res = sf.register(req);
 
-        assertThrows(Exception.class, () -> serverFacade.register(req)); // register twice
+        assertThrows(Exception.class, () -> sf.register(req)); // register twice
+    }
+
+
+    @Test
+    public void loginPositive() throws Exception{
+        sf.register(new RegisterRequest("log1", "pass", "email"));
+
+        var res = sf.login(new LoginRequest("log1", "pass"));
+
+        assertNotNull(res.authToken());
+    }
+
+    @Test
+    public void loginNegative() throws Exception{
+        assertThrows(Exception.class, () -> sf.login(new LoginRequest("no", "no")));
     }
 
 }
