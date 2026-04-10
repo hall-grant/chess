a2a85e09506ba7a191a5c8ea91664d3e9a48af58
hall-grant <hall.grant.david@gmail.com>
2026-04-09 19:11:26 -0600

    Copied and translated petshop's connectionManager class

---
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -0,0 +1,68 @@
+package server.websocket;
+
+import com.google.gson.Gson;
+import io.javalin.websocket.WsContext;
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectionManager {
+    private final Map<Integer, Set<WsContext>> connections = new ConcurrentHashMap<>();
+    private final Gson gson = new Gson();
+
+    public void add(int gameID, WsContext ctx) {
+
+        Set<WsContext> c = connections.get(gameID);
+        if(c == null){
+            c = ConcurrentHashMap.newKeySet();
+            connections.put(gameID, c);
+        }
+        c.add(ctx); // should be assigned to same object, not copy
+    }
+
+    public void remove(int gameID, WsContext ctx) {
+
+        Set<WsContext> set = connections.get(gameID);
+        if(set != null){
+            set.remove(ctx);
+        }
+    }
+
+    public void broadcastAll(int gameID, Object message) {
+
+        Set<WsContext> set = connections.get(gameID);
+        if(set == null){
+            return;
+        }
+
+        String string = gson.toJson(message);
+
+        for (var c  : set){
+            if(c.session.isOpen()){
+                c.send(string);
+            }
+        }
+    }
+
+    public void broadcastExclude(WsContext ctx, int gameID, Object message){
+
+        Set<WsContext> set = connections.get(gameID);
+        if(set == null){
+            return;
+        }
+
+        String string = gson.toJson(message);
+        for (var c : set){
+            if(c.session.isOpen()){
+                if(c == ctx){
+                    return;
+                }
+                c.send(string);
+            }
+        }
+    }
+
+}
